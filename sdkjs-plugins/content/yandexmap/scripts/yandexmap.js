ee50560605a344265bf41a65c6be9f7099b70e64
Dmitry Laptev <Dmitry.Laptev@avsmedia.net>
2024-09-12 18:16:10 +0300

    3com

--- sdkjs-plugins/content/yandexmap/scripts/yandexmap.js
+++ sdkjs-plugins/content/yandexmap/scripts/yandexmap.js
@@ -1,12 +1,33 @@
 (function (window,undefined)
 {
-  window.Asc.plugin.init = function ()
+  let userAddress = document.getElementById("userSearch").value
+  let windowStatus = false
+
+  function startMap ()
   {
-    function startMap ()
-    {
-      newMap = new ymap("map",{center:[55,45],zoom:10},{searchControlProvider: "yandex#search"})
-    }
+    newMap = new ymap("map",{center:[55.74, 37.58],zoom:10},{searchControlProvider: "yandex#search"})
+    newMap.controls.add(searchControl)
+    searchControl.search(userAddress)
+  }
+
+  window.Asc.plugin.init = function () {
     ymap.ready(startMap)
+
+    if (e.keyCode == 13) {
+      document.getElementById("button_id").onclick
+    }
+
+    if (this.info.isViewMode) {
+      document.getElementById("userSearch").disable = true
+      document.getElementById("button_id").disable = true
+    }
+
+    if (!windowStatus) {
+      let player = document.getElementById("id_player")
+      player.innerHTML = "<div id=\"map\" style=\"width: 600px; height: 400px\"></div>";
+      windowStatus = true;
+      window.Asc.plugin.resizeWindow(620, 480, 390, 400, 0, 0)
+    }
   }
 })(window,undefined)
 
