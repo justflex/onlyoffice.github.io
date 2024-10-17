0fcc5b53f1d0e27f8842ff21261a0f3eeff39dc5
justflex <laptev-dima163@mail.ru>
2024-10-17 17:38:18 +0300

    add icons and tranlations

--- sdkjs-plugins/content/yandexmapV2/scripts/inputSettings.js
+++ sdkjs-plugins/content/yandexmapV2/scripts/inputSettings.js
@@ -33,14 +33,22 @@ let userAddress = '';
 
   localStorage.setItem('autoEnter_yandex_map_item', '0')
 
-  window.Asc.plugin.onTranslate = function () {
+  window.Asc.plugin.onTranslate = function ()
+  {
     let lab = document.querySelector("label");
     let inp = document.querySelector("input");
-    if (lab) {
+    let lab_err = document.getElementById('label_error');
+    if(lab)
+    {
       lab.innerHTML = window.Asc.plugin.tr("Your address");
     }
-    if (inp) {
+    if(inp)
+    {
       inp.placeholder = window.Asc.plugin.tr("Enter your address and press Enter");
+    }
+    if(lab_err)
+    {
+      lab_err.innerHTML = window.Asc.plugin.tr("The field is empty");
     }
   }
 
